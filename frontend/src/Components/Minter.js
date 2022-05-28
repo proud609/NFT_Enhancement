b5aa39f35f7299ff49810d471135b53a7222a92e
morris1961 <b07705044@ntu.edu.tw>
2022-05-28 22:38:16 +0800

    add loading upgrade, loading refresh, disable upgrade

--- frontend/src/Components/Minter.js
+++ frontend/src/Components/Minter.js
@@ -1,16 +1,19 @@
 import { ProCard } from '@ant-design/pro-card';
 import '@ant-design/pro-card/dist/card.css';
 import { Button, Empty, Spin, Card, Typography, Row, Col } from 'antd';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { LoadingOutlined } from "@ant-design/icons";
 import { upgrade, cheat, refresh } from '../utils/interact';
 const { Title } = Typography;
 
 const Minter = ({ setData, isOwner, data, fetching, successMessage, errorMessage, warningMessage }) => {
 
-  const [refreshing, setRefreshing] = useState(false);
+  const [refreshing, setRefreshing] = useState();
+  const [upgradeing, setUpgradeing] = useState(false);
+  const [cheating, setCheating] = useState(false);
 
   const onUpgrade = async (tokenId) => {
+    setUpgradeing(true);
     const { success, status } = await upgrade(tokenId);
     if (success) {
       successMessage(status);
@@ -18,9 +21,11 @@ const Minter = ({ setData, isOwner, data, fetching, successMessage, errorMessage
     else {
       errorMessage(status)
     }
+    setUpgradeing(false);
   }
 
   const onCheat = async (tokenId) => {
+    setCheating(true);
     const { success, status } = await cheat(tokenId);
     if (success) {
       successMessage(status);
@@ -28,10 +33,13 @@ const Minter = ({ setData, isOwner, data, fetching, successMessage, errorMessage
     else {
       errorMessage(status)
     }
+    setCheating(false);
   }
 
   const onRefresh = async (tokenId, i) => {
-    setRefreshing(true);
+    let prevRefreshing = [...refreshing]
+    prevRefreshing[i] = true
+    setRefreshing(prevRefreshing);
     const { success, json, status } = await refresh(tokenId);
     if (success) {
       successMessage(status);
@@ -42,17 +50,26 @@ const Minter = ({ setData, isOwner, data, fetching, successMessage, errorMessage
     else {
       errorMessage(status)
     }
-    setRefreshing(false)
+    prevRefreshing[i] = false
+    setRefreshing(prevRefreshing);
   }
+
+  useEffect(() => {
+    setRefreshing(Array(data.length).fill(false))
+  }, [data])
+
   return (
     <>
       {data.length > 0 ?
         data.map((weapon, i) => {
+          console.log(i)
+          console.log(refreshing[i])
           return (
-            <Spin spinning={refreshing} key={i}>
+            <Spin spinning={refreshing[i]} key={i}>
               <ProCard split="vertical" bordered>
                 <ProCard colSpan="33%">
                   <iframe src={weapon.animation_url}
+                    title={weapon.tokenId}
                     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                     frameBorder="0" sandbox="allow-scripts"
                     style={{ "minHeight": "500px", "width": "400px" }}>
@@ -74,13 +91,28 @@ const Minter = ({ setData, isOwner, data, fetching, successMessage, errorMessage
                   <br />
                   <Row>
                     <Col span={4} >
-                      <Button type="primary" size="large" shape="round" onClick={() => onUpgrade(weapon.tokenId)}>upgrade</Button>
+                      <Button
+                        type="primary"
+                        size="large"
+                        shape="round"
+                        loading={upgradeing}
+                        disabled={weapon.attributes[0].value === 0}
+                        onClick={() => onUpgrade(weapon.tokenId)}>upgrade</Button>
                     </Col>
                     <Col span={4} >
-                      <Button type="primary" size="large" shape="round" onClick={() => onRefresh(weapon.tokenId, i)}>refresh</Button>
+                      <Button
+                        type="primary"
+                        size="large"
+                        shape="round"
+                        onClick={() => onRefresh(weapon.tokenId, i)}>refresh</Button>
                     </Col>
                     <Col span={4} >
-                      <Button type="primary" size="large" shape="round" onClick={() => onCheat(weapon.tokenId)} disabled={!isOwner}>cheat</Button>
+                      <Button type="primary"
+                        size="large"
+                        shape="round"
+                        onClick={() => onCheat(weapon.tokenId)}
+                        loading={cheating}
+                        disabled={!isOwner}>cheat</Button>
                     </Col>
                   </Row>
                 </ProCard>
